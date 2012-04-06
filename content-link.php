21ad7da352ff6aa3f17acd193adbf824fe21dbe1
Montana Flynn <montana@montanaflynn.me>
2012-04-06 03:13:05 +0000

    Switch to custom field instad of content for outbound links to work better

--- content-link.php
+++ content-link.php
@@ -5,14 +5,14 @@
  * Learn more: http://codex.wordpress.org/Post_Formats
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 0.5.0
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="entry-content">
-			<h2><a href="<?php the_content(); ?>"><?php the_title(); ?></a></h2>
-		</div><!-- .entry-content -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-content">
+		<h2><a href="<?php echo get_post_meta($post->ID, 'link-url', true); ?>"><?php the_title(); ?></a></h2>
+	</div><!-- .entry-content -->
 
-	</article><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-<?php the_ID(); ?> -->
